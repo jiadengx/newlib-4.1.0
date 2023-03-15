d5a5f1c81462df17aeeff5768e439ff8b4028bb8
dengjianbo <dengjianbo@loongson.cn>
2023-03-15 17:44:44 +0800

    Add rename syscall and execve syscall
    enable rename for loongarch in configure.host

--- libgloss/loongarch/sys_execve.c
+++ libgloss/loongarch/sys_execve.c
@@ -1,11 +1,10 @@
 #include <machine/syscall.h>
 #include "internal_syscall.h"
 
-/* Transfer control to a new process. Minimal implementation for a
-   system without processes from newlib documentation.  */
+/* Transfer control to a new process. */
+
 int
 _execve(const char *name, char *const argv[], char *const env[])
 {
-  errno = ENOMEM;
-  return -1;
+  return syscall_errno (SYS_execve, 3, name, argv, env, 0, 0, 0);
 }
