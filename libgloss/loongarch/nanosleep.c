c51a9e29320f749d4136d449be0cedb715a2436b
dengjianbo <dengjianbo@loongson.cn>
2023-03-16 10:40:07 +0800

    remove aboselte syscalls ftime and adjust nanosleep

--- libgloss/loongarch/nanosleep.c
+++ libgloss/loongarch/nanosleep.c
@@ -1,12 +1,9 @@
-#include <machine/syscall.h>
 #include <sys/time.h>
+#include <machine/syscall.h>
+#include "internal_syscall.h"
 
 int
 nanosleep(const struct timespec *rqtp, struct timespec *rmtp)
 {
-//  unsigned long current_time, end_time;
-//  asm ("rdtime %0" : "+r" (current_time));
-//  end_time = current_time + rqtp->tv_sec * 1000000000ULL + rqtp->tv_nsec;
-//  while (current_time <= end_time) asm ("rdtime %0" : "+r" (current_time));
-  return 0;
+  return syscall_errno (SYS_nanosleep, 2, rqtp, rmtp, 0, 0, 0, 0);
 }
